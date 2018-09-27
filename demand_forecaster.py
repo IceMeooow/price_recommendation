01241b23fe9fcb98141cd6dc1bccfe52cf60e2b3
Krokhtiak Iryna <iryna.krokhtiak@gmail.com>
2018-09-27 14:53:46 +0300

    renamed:    forecaster.py -> demand_forecaster.py

--- demand_forecaster.py
+++ demand_forecaster.py
@@ -9,6 +9,7 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
+from keras.callbacks import EarlyStopping
 
 
 class Forecaster:
@@ -16,7 +17,7 @@ class Forecaster:
     def __init__(self, path):
         self.path = path
 
-    def create_data(self):
+    def create_and_prepare_full_data(self):
         xlsx_file = pd.ExcelFile(self.path)
 
         store_lookup = pd.read_excel(xlsx_file, sheet_name=xlsx_file.sheet_names[1], header=1, usecols=8)
@@ -27,31 +28,18 @@ class Forecaster:
         df = df.rename(columns={'STORE_NUM': 'STORE_ID'})
         df = pd.merge(df, store_lookup, how='inner', on='STORE_ID')
         df = df.drop(['PARKING_SPACE_QTY'], axis=1)
+        df = df.dropna(axis=0)
         return df
 
-    def fill_nan_in_price(self, data):
-        for idx in data.index:
-            if (pd.isna(data.loc[idx, 'BASE_PRICE'])) and (not pd.isna(data.loc[idx, 'PRICE'])):
-                data.loc[idx, 'BASE_PRICE'] = data.loc[idx, 'PRICE']
-
-            if (pd.isna(data.loc[idx, 'PRICE'])) and (not pd.isna(data.loc[idx, 'BASE_PRICE'])):
-                data.loc[idx, 'PRICE'] = data.loc[idx, 'BASE_PRICE']
-                if data.loc[idx, 'UNITS'] != 0:
-                    data.loc[idx, 'SPEND'] = data.loc[idx, 'UNITS'] * data.loc[idx, 'PRICE']
-        return data
-
     def recommend_price(self, data):
-        product = input('enter a product name... ').upper()
+        product_upc = int(input('enter a product UPC code... '))
         store = input('enter a store name... ').upper()
-        period = int(input('enter the number of weeks (the price will be found for this period) ...'))
 
-        product_history_in_store = self._prepare_product_data(data, product, store)
+        product_history_in_store = self._prepare_product_data(data, product_upc, store)
 
-        train_data, test_data = self._split_data_on_train_test_set(product_history_in_store, 75)
+        samples, labels = self._split_data_on_samples_and_labels(product_history_in_store)
 
-
-        train_set, test_set, train_labels, test_labels = self._split_data_on_samples_and_labels(train_data,
-                                                                                                     test_data, (-period))
+        train_set, test_set, train_labels, test_labels = self._split_data_on_train_test_set(samples, labels, 80)
 
         scaler_dict, encoded_train_set = self._encode_train_set(train_set)
 
@@ -62,54 +50,55 @@ class Forecaster:
 
         lstm_model = self._lstm_neural_network_model(100, X_train.shape[1], X_train.shape[2])
 
+        early_stopping = EarlyStopping(patience=3)
         history = lstm_model.fit(X_train,
                                  train_labels,
-                                 epochs=500,
+                                 epochs=2000,
                                  batch_size=50,
                                  validation_data=(X_test, test_labels),
+                                 callbacks=[early_stopping],
                                  verbose=2,
                                  shuffle=False)
 
-        last_data_in_product_history = train_data.loc[train_data.index[(-period):].values]
-        encoded_history = self._encode_test_set(last_data_in_product_history, scaler_dict)
-        encoded_history = self._reshape_data(encoded_history)
-        prediction_for_data = self._predict_labels(lstm_model, encoded_history)
-        return prediction_for_data
+        # last_data_in_product_history = train_data.loc[train_data.index[-2:].values]
+        # encoded_history = self._encode_test_set(last_data_in_product_history, scaler_dict)
+        # encoded_history = self._reshape_data(encoded_history)
+        # prediction_for_data = self._predict_labels(lstm_model, encoded_history)
+        return history
 
-    def _prepare_product_data(self, data, product_name, store_name):
-        history = data[(data.DESCRIPTION == product_name) & (data.STORE_NAME == store_name)]
+    def _prepare_product_data(self, data, product_upc, store_name):
+        history = data[(data.UPC == product_upc) & (data.STORE_NAME == store_name)]
         history.index = history['WEEK_END_DATE']
         history = history.drop('WEEK_END_DATE', axis=1)
         history.sort_index(inplace=True)
         return history
 
-    def _split_data_on_train_test_set(self, data, train_size_in_percent):
+    def _split_data_on_samples_and_labels(self, data):
+        labels = pd.DataFrame()
+        labels['u-1'] = data.UNITS.shift(-1)
+        labels['u-2'] = data.UNITS.shift(-2)
+
+        data = data.drop(['UNITS'], axis=1)
+        samples = data.drop(labels.index[-2:], axis=0)
+        labels = labels.dropna(axis=0)
+        return samples, labels
+
+    def _split_data_on_train_test_set(self, data, labels, train_size_in_percent):
         train_size = int(len(data) * train_size_in_percent / 100)
         train_set, test_set = data[:train_size], data[train_size:]
-        return train_set, test_set
-
-    def _split_data_on_samples_and_labels(self, train_data, test_data, periods):
-        train_labels = train_data.BASE_PRICE.shift(periods)
-        test_labels = test_data.BASE_PRICE.shift(periods)
-
-        train_set = train_data.drop(train_data.index[periods:], axis=0)
-        train_labels = train_labels.drop(train_labels.index[periods:], axis=0)
-
-        test_set = test_data.drop(test_labels.index[periods:], axis=0)
-        test_labels = test_labels.drop(test_labels.index[periods:], axis=0)
+        train_labels, test_labels = labels[:train_size], labels[train_size:]
         return train_set, test_set, train_labels, test_labels
 
     def _encode_train_set(self, train_set):
         encoded_cat_columns = self._encode_cat_column(train_set, ['ADDRESS_CITY_NAME',
-                                                                        'ADDRESS_STATE_PROV_CODE',
-                                                                        'MSA_CODE',
-                                                                        'SEG_VALUE_NAME'])
+                                                                  'ADDRESS_STATE_PROV_CODE',
+                                                                  'MSA_CODE',
+                                                                  'SEG_VALUE_NAME'])
         scaler_dict, scaled_num_columns = self._scale_num_column(train_set,
                                                                        ['SALES_AREA_SIZE_NUM',
                                                                         'AVG_WEEKLY_BASKETS',
                                                                         'BASE_PRICE',
                                                                         'PRICE',
-                                                                        'UNITS',
                                                                         'VISITS',
                                                                         'HHS',
                                                                         'SPEND'])
@@ -127,7 +116,6 @@ class Forecaster:
                        'AVG_WEEKLY_BASKETS',
                        'BASE_PRICE',
                        'PRICE',
-                       'UNITS',
                        'VISITS',
                        'HHS',
                        'SPEND']:
@@ -151,7 +139,7 @@ class Forecaster:
         random.seed(random_state)
         model = Sequential()
         model.add(LSTM(number_of_neurons, input_shape=(first_parameter_of_shape, second_parameter_of_shape)))
-        model.add(Dense(1))
+        model.add(Dense(2))
         model.compile(loss='mae', optimizer='adam')
         return model
 
